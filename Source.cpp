9ca61eeac6468562186d5be54397d129fb5753c9
Rishav Raj <53914807+Rishav-api@users.noreply.github.com>
2019-12-03 02:42:56 +0530

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -5,28 +5,31 @@
 
 using namespace std;
 
-int t = 50,x,y,a;
+int t = 50, x, y, a;
+int counter = 0;
 
 class species {
 public:
-	int vision, stamina, a1=10, food;
+	int vision, stamina, a1 = 10, food;
+	string status;
 	pair<int, int>coord;
 	species() {
 		vision = 5;
 		stamina = 50;
 		food = 0;
+		status = "ALIVE";
 	}
-	species(int v,int s) {
+	species(int v, int s) {
 		vision = v;
 		stamina = s;
 	}
 };
 
-int move(int,int,int,int);
+int move(int, int, int, int);
 list<species>skills;
 
 class environment {
-	int population = 10, food = 400, size=100;
+	int population = 10, food = 400, size = 100;
 
 public:
 	int location[102][102];
@@ -53,15 +56,23 @@ void environment::day() {
 		}
 	}
 	while (food--) {
-		location[rand() % size + 1][rand() % size + 1]=1;
+		location[rand() % size + 1][rand() % size + 1] = 1;
 	}
 	x = 1;
-	for (list<species>::iterator i = skills.begin(); i != skills.end();i++,x++) {
+	for (list<species>::iterator i = skills.begin(); i != skills.end(); i++, x++) {
 		switch (x) {
-		case 1: (*i).coord = make_pair(0, rand() % size + 1); break;
-		case 2: (*i).coord = make_pair(size + 1, rand() % size + 1); break;
-		case 3: (*i).coord = make_pair(rand() % size + 1,0); break;
-		case 4: (*i).coord = make_pair(rand() % size + 1, size + 1); break;
+		case 1:
+			(*i).coord = make_pair(0, rand() % size + 1);
+			break;
+		case 2:
+			(*i).coord = make_pair(size + 1, rand() % size + 1);
+			break;
+		case 3:
+			(*i).coord = make_pair(rand() % size + 1, 0);
+			break;
+		case 4:
+			(*i).coord = make_pair(rand() % size + 1, size + 1);
+			break;
 		}
 		if (x == 4) {
 			x = 0;
@@ -72,85 +83,94 @@ void environment::day() {
 	}*/
 	while (t--) {
 		for (list<species>::iterator i = skills.begin(); i != skills.end(); i++) {
+			if ((*i).status == "DEAD") {
+				continue;
+			}
 			x = ((*i).coord).first;
 			y = ((*i).coord).second;
 			//cout << x << " " << y << " ";
 			while (true) {
-				a = rand() % 8 + 1;
-				if (a != (*i).a1) {
+				a = rand() % 7;
+				if ((a + 4) % 8 != (*i).a1) {
 					switch (a) {
 					case 1:
 						if ((--y) > 0) {
-							(*i).a1 = a;
-							--((*i).coord).second;
+							(*i).a1 = (a + 4) % 8;
+							--(((*i).coord).second);
 						}
 						break;
 					case 2:
 						if ((--y) > 0 && (++x) < (size + 1)) {
-							(*i).a1 = a;
-							--((*i).coord).second;
-							++((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							--(((*i).coord).second);
+							++(((*i).coord).first);
 						}
 						break;
 					case 3:
 						if ((++x) < (size + 1)) {
-							(*i).a1 = a;
-							++((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							++(((*i).coord).first);
 						}
 						break;
 					case 4:
 						if ((++y) < (size + 1) && (++x) < (size + 1)) {
-							(*i).a1 = a;
-							++((*i).coord).second;
-							++((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							++(((*i).coord).second);
+							++(((*i).coord).first);
 						}
 						break;
 					case 5:
 						if ((++y) < (size + 1)) {
-							(*i).a1 = a;
-							++((*i).coord).second;
+							(*i).a1 = (a + 4) % 8;
+							++(((*i).coord).second);
 						}
 						break;
 					case 6:
 						if ((++y) < (size + 1) && (--x) > 0) {
-							(*i).a1 = a;
-							++((*i).coord).second;
-							--((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							++(((*i).coord).second);
+							--(((*i).coord).first);
 						}
 						break;
 					case 7:
 						if ((--x) > 0) {
-							(*i).a1 = a;
-							--((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							--(((*i).coord).first);
 						}
 						break;
-					case 8:
+					case 0:
 						if ((--x) > 0 && (--y) > 0) {
-							(*i).a1 = a;
-							--((*i).coord).second;
-							--((*i).coord).first;
+							(*i).a1 = (a + 4) % 8;
+							--(((*i).coord).second);
+							--(((*i).coord).first);
 						}
 						break;
 					}
-					if ((*i).a1 == a) {
+					if ((*i).a1 == (a + 4) % 8) {
 						break;
 					}
 				}
 			}
-			if (location[x][y] == 1) {
+			//cout << ((*i).coord).first << " " << ((*i).coord).second << " ";
+			//cout << endl;
+			if (location[((*i).coord).first][((*i).coord).second] == 1) {
 				++((*i).food);
-				--location[x][y];
+				--location[((*i).coord).first][((*i).coord).second];
 			}
 			--((*i).stamina);
 			if ((*i).stamina == 0 && ((*i).food) == 0) {
-			//	skills.erase(i);
+				(*i).status = "DEAD";
 			}
 		}
 	}
-	cout << 990;
+	for (list<species>::iterator i = skills.begin(); i != skills.end(); i++) {
+		cout << (*i).food << " ";
+	}
+
 }
 
 int main() {
 	srand(time(NULL));
 	environment obj1;
+	cout << "\nDolesh 1901CS20\nRishav 1901CS46\n";
 }
